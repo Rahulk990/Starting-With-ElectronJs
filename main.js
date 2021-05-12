5b163cb4d8c5854c9e6cb3700d83d365cc6a02d0
Rahul <rahulv0530@gmail.com>
2021-05-12 18:58:21 +0530

    Clear

--- main.js
+++ main.js
@@ -12,26 +12,7 @@ function createWindow() {
   mainWindow.loadFile('index.html')
   mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => { mainWindow = null })
-
-  mainWindow.webContents.on('did-finish-load', () => {
-    mainWindow.webContents.send('response', "From the webContents")
-  })
 }
-
-ipcMain.on('sync-message', (e, args) => {
-  console.log(args)
-
-  setTimeout(() => {
-    e.returnValue = 'A sync response from the main process'
-  }, 2000)
-})
-
-ipcMain.on('channel1', (e, args) => {
-  console.log(args);
-  setTimeout(() => {
-    e.sender.send('response', "Message Received");
-  }, 4000)
-})
 
 app.on('ready', () => {
   createWindow()
